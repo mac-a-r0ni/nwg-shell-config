86c6b232fa7f7895567a6e458d35df9cd9ca90b3
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2024-02-05 04:15:09 -0500

    add slackware defaults

--- nwg_shell_config/locker.py
+++ nwg_shell_config/locker.py
@@ -54,9 +54,9 @@ defaults = {
     "gtklock-logout-command": "swaymsg exit",
     "gtklock-playerctl": False,
     "gtklock-powerbar": False,
-    "gtklock-poweroff-command": "systemctl -i poweroff",
-    "gtklock-reboot-command": "systemctl reboot",
-    "gtklock-suspend-command": "systemctl suspend",
+    "gtklock-poweroff-command": "loginctl -i poweroff",
+    "gtklock-reboot-command": "loginctl reboot",
+    "gtklock-suspend-command": "loginctl suspend",
     "gtklock-time-format": "%H:%M:%S",
     "gtklock-userinfo": False,
     "gtklock-userswitch-command": ""
